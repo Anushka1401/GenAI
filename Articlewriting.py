cfffb26ad467f512a196371edf03a922469c4f8d
Anushka Sanjeev <63353818+Anushka1401@users.noreply.github.com>
2024-01-08 17:44:14 +0530

    Update Articlewriting.py

--- Articlewriting.py
+++ Articlewriting.py
@@ -1,12 +1,7 @@
 import openai
 import streamlit as st
-import os
 
-# Set your OpenAI API key as an environment variable
-os.environ["OPENAI_API_KEY"] = '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'
-
-# Create OpenAI client
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = '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'
 
 def main():
     st.title("Article Writer")
@@ -14,14 +9,14 @@ def main():
     content = "I want you to write a short literature review on the topic " + notes
     if st.button("Generate Article"):
         with st.spinner("Generating Article..."):
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
-                          {'role': 'user', 'content': content}]
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=content,
+                max_tokens=200
             )
-        description = response['choices'][0]['message']['content']
+        description = response["choices"][0]["text"]
         st.subheader("Generated Writeup:")
         st.write(description)
 
 if __name__ == '__main__':
-    main()
+   main()
